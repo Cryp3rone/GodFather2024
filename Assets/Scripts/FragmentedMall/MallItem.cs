aa43b08213c51538cacb9e9b183b8902a38fe949
Clément THOMAS <clemto95@live.fr>
2024-09-11 13:54:05 +0200

    [ADD] fragmented mall

---
+++ Assets/Scripts/FragmentedMall/MallItem.cs
@@ -0,0 +1,9 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public class MallItem
+{
+    public SpriteRenderer SpriteRenderer;
+    public int Index;
+}
---
+++ Assets/Scripts/FragmentedMall/MallManager.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MallManager : MonoBehaviour
+{
+    public static MallManager Instance;
+
+    public event Action<Sprite, int> OnQuestionAnswer;
+
+    [SerializeField] private List<MallItem> _mallParts = new List<MallItem>();
+
+    private void Awake()
+    {
+        if(Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    private void OnEnable()
+    {
+        OnQuestionAnswer += ShowPart;
+    }
+
+    private void OnDisable()
+    {
+        OnQuestionAnswer -= ShowPart;
+    }
+
+    private void ShowPart(Sprite sprite, int index)
+    {
+        foreach (MallItem item in _mallParts)
+        {
+            if(item.Index == index)
+            {
+                item.SpriteRenderer.sprite = sprite;
+                return;
+            }
+        }
+    }
+}
